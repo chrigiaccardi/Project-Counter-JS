6898e340b123dc394132d06104c4d9564ac2ad2f
Christian Giaccardi <christianerita@Mac.lan>
2025-11-11 08:02:13 +0100

    ottimizzazione DRY

--- js/project.js
+++ js/project.js
@@ -20,114 +20,80 @@ dispositivo.classList.add("device")
 // 6.0 Bottoni
 const buttonContainer = document.createElement("div")
 
+    // 6.1 Funzione Bottoni
+    function creaBottone(text,classe,ariaLabel,titolo) {
+        const button = document.createElement("button")
+        button.textContent = text
+        button.classList.add(classe, "btnDispositivo")
+        button.setAttribute("aria-label", ariaLabel)
+        button.title = titolo
+        return button
+    }
+
+
     // 6.1 Bottone Più (+)
-    const btnPlus = document.createElement("button")
-    btnPlus.textContent = "Più (+)"
-    btnPlus.classList.add("verde", "btnDispositivo")
-    btnPlus.setAttribute("aria-label", "Incrementa il contatore di uno")
-    btnPlus.title = "Incrementa il contatore di uno"
+    const btnPlus = creaBottone("Più (+)","verde","Incrementa il contatore di uno","Incrementa il contatore di uno")    
 
     // 6.2 Bottone Meno (-)
-    const btnMinus = document.createElement("button")
-    btnMinus.textContent = "Meno (-)"
-    btnMinus.classList.add("rosso", "btnDispositivo")
-    btnMinus.setAttribute("aria-label", "Decrementa il contatore di uno")
-    btnMinus.title = "Decrementa il contatore di uno"
+    const btnMinus = creaBottone("Meno (-)","rosso","Decrementa il contatore di uno","Decrementa il contatore di uno")
 
     // 6.3 Bottone Reset (0)
-    const btnReset = document.createElement("button")
-    btnReset.textContent = "Reset (0)"
-    btnReset.classList.add("grigio", "btnDispositivo")
-    btnReset.setAttribute("aria-label", "Resetta il valore a zero")
-    btnReset.title = "Resetta il valore a zero"
+    const btnReset = creaBottone("Reset (0)","grigio","Resetta il valore a zero","Resetta il valore a zero")
 
     // 6.4 Bottone Più 5 (+5)
-    const btnPlus5 = document.createElement("button")
-    btnPlus5.textContent = "+5"
-    btnPlus5.classList.add("verdeScuro", "btnDispositivo")
-    btnPlus5.setAttribute("aria-label", "Incrementa il contatore di cinque")
-    btnPlus5.title = "Incrementa il contatore di cinque"
+    const btnPlus5 = creaBottone("+5","verdeScuro","Incrementa il contatore di cinque","Incrementa il contatore di cinque")
 
     // 6.5 Bottone Salva Valore
-    const btnSave = document.createElement("button")
-    btnSave.textContent = "Salva"
-    btnSave.classList.add("blu", "btnDispositivo")
-    btnSave.setAttribute("aria-label", "Salva il valore")
-    btnSave.title = "Salva il valore"
+    const btnSave = creaBottone("Salva","blu","Salva il valore","Salva il valore")
 
     // 6.6 Bottone Meno 5 (-5)
-    const btnMinus5 = document.createElement("button")
-    btnMinus5.textContent = "-5"
-    btnMinus5.classList.add("marrone", "btnDispositivo")
-    btnMinus5.setAttribute("aria-label", "Decrementa il contatore di cinque")
-    btnMinus5.title = "Decrementa il contatore di cinque"
+    const btnMinus5 = creaBottone("-5","marrone","Decrementa il contatore di cinque","Decrementa il contatore di cinque")
 
 
 // 7.0 AppendChild Dispositivo
 container.appendChild(dispositivo)
+dispositivo.append(display,buttonContainer)
+buttonContainer.append(btnPlus,btnReset,btnMinus,btnPlus5,btnMinus5,btnSave)
 
-dispositivo.appendChild(display)
-dispositivo.appendChild(buttonContainer)
-
-buttonContainer.appendChild(btnPlus)
-buttonContainer.appendChild(btnReset)
-buttonContainer.appendChild(btnMinus)
-buttonContainer.appendChild(btnPlus5)
-buttonContainer.appendChild(btnMinus5)
-buttonContainer.appendChild(btnSave)
-
-
-
-// 8.0 Funzione
+// 8.0 Funzioni
 display.textContent = `Numero: 0`
 function aggiornaDisplay() {
     display.textContent = `Numero: ${i}`
 }
-
+function aggiornaValore(valore) {
+    i += valore;
+    aggiornaDisplay()
+}
+function resettaValore() {
+    i = 0;
+    aggiornaDisplay()
+}
 
 
 // 9.0 Eventi
 
     // 9.1 Click Bottone Più
-    btnPlus.addEventListener("click", button => {
-        i++;
-        aggiornaDisplay()
-    })
+    btnPlus.addEventListener("click", button => { aggiornaValore(1)})
 
     // 9.2 Click Bottone Meno
-    btnMinus.addEventListener("click", button => {
-        i--;
-        aggiornaDisplay()
-
-    })
+    btnMinus.addEventListener("click", button => { aggiornaValore(-1)})
 
     // 9.3 Click Bottone Reset
-    btnReset.addEventListener("click", button => {
-        i = 0;
-        aggiornaDisplay()
-
-    })
+    btnReset.addEventListener("click", button => {resettaValore()})
 
     // 9.4 Click Bottone Più 5
-
-    btnPlus5.addEventListener("click", button => {
-        i += 5;
-        aggiornaDisplay()
-
-    })
+    btnPlus5.addEventListener("click", button => { aggiornaValore(5)})
 
     // 9.5 Click Bottone Meno 5
-    btnMinus5.addEventListener("click", button => {
-        i -= 5;
-        aggiornaDisplay()
-
-    })
+    btnMinus5.addEventListener("click", button => { aggiornaValore(-5)})
 
     // 9.6 Click Bottone Salva - Creazione Tabella
     btnSave.addEventListener("click", () => {
         if (!savedTable) {
             savedTable = document.createElement("table")
             savedTable.classList.add("tabella")
+
+            // Funzioni Creazione Tabella
 
             // 9.6.1 Creazione Elementi Tabella
             const header = document.createElement("tr")
@@ -139,11 +105,8 @@ function aggiornaDisplay() {
             th3.textContent = ""
 
             // 9.6.2 Append Child Tabella
-            header.appendChild(th1)
-            header.appendChild(th2)
-            header.appendChild(th3)
+            header.append(th1,th2,th3)
             savedTable.appendChild(header)
-
             container.appendChild(savedTable)
             
         };
@@ -163,11 +126,8 @@ function aggiornaDisplay() {
             const removeCell = document.createElement("td")
 
             // 9.6.6 Creazione Bottone Elimina Riga
-            const btnRemove = document.createElement("button")
-            btnRemove.textContent = "Elimina"
-            btnRemove.classList.add("btnRemove")
-            btnRemove.setAttribute("aria-label", "Elimina questo valore salvato")
-            btnRemove.title = "Elimina questo valore salvato"
+            const btnRemove = creaBottone("Elimina", "btnRemove", "Elimina questo valore salvato", "Elimina questo valore salvato")
+            btnRemove.classList.remove("btnDispositivo")
             btnRemove.addEventListener("click", () => {
                 row.remove();
                 if (savedTable.rows.length === 1) {
@@ -179,11 +139,11 @@ function aggiornaDisplay() {
             // 9.6.7 AppendChild Elementi alla tabella
             removeCell.appendChild(btnRemove)
             inputCell.appendChild(input)
-            row.appendChild(valueCell)
-            row.appendChild(inputCell)
-            row.appendChild(removeCell)
+            row.append(valueCell,inputCell,removeCell)
             savedTable.appendChild(row)
     });
 
 
 
+
+ 
